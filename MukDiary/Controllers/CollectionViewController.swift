7812d4e78b17fcc80485d52447f90af5c56cbcc3
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-02-26 19:52:21 +0900

    feat : Connect CollectionView to DetailViewController

--- MukDiary/Controllers/CollectionViewController.swift
+++ MukDiary/Controllers/CollectionViewController.swift
@@ -9,28 +9,34 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+//    {
+//        if segue.identifier == "mySegue"
+//        {
+//            let viewController = segue.destination as! AnimalsViewController1
+//            print("In \(#function), valueToPass = \(valueToPass)")
+//            viewController.dataLabelObject = valueToPass
+//        }
+//    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.collectionView.reloadData()
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let cell = sender as? UICollectionViewCell, let indexPath = collectionView.indexPath(for: cell) {
+            if let vc = segue.destination as? DetailViewController {
+                vc.diary = DataManager.shared.diaryList[indexPath.row]
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-
-
+        self.collectionView.allowsSelection = true
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: - UICollectionViewDataSource
 
@@ -52,34 +58,12 @@ class CollectionViewController: UICollectionViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+//
+//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        performSegue(withIdentifier: "detailViewSegue", sender: self)
+//
+//    }
     
-    }
-    */
+
 
 }
